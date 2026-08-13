4249b8497c8bb49cdeccfdc680eae8fc376510a2
enscvs <128850857+enscvs@users.noreply.github.com>
2026-08-13 18:23:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,27 +4,17 @@ APP JS
 ========================================================
 */
 
-if (
-  req.method === "GET" &&
-  pathname === "/app.js"
-) {
+require("dotenv").config();
 
-  const filePath =
-    path.join(
-      __dirname,
-      "public",
-      "app.js"
-    );
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
+const OpenAI = require("openai");
 
-  return serveFile(
-    res,
-    filePath,
-    "application/javascript; charset=utf-8"
-  );
-
-}
-
+const {
+  Client,
+} = require("@modelcontextprotocol/sdk/client/index.js");
 
 /*
 ========================================================
